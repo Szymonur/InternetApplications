b0cf8b3f04f44f744de93417e28527f1ba8dbcaf
Szymon Urban <szymonur88828@gmail.com>
2026-06-05 14:28:22 +0200

    ass 6

--- Assigment6/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ Assigment6/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+056ab972537c7dfce549162ed43f6dd28e248fe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c016fe3e1d1f5228a748960096d46c331e839d56")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
